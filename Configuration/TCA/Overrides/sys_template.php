2abdcad219ccdbf554c1326dcf4e9ee45faa86a4
Thomas Lüder <t.lueder@nets.de>
2023-06-22 17:11:22 +0200

    Delete everything

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('TYPO3') or exit;
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-  'formhandler',
-  'Configuration/TypoScript/Json',
-  'Activate JSON Output (optional)'
-);
