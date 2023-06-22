1045f36883b5a0baf11164b92e14507f46c86bd2
Thomas Lüder <t.lueder@nets.de>
2023-06-22 18:09:35 +0200

    Setup new extension scaffold

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+$EM_CONF['formhandler'] = [
+  'title' => 'Formhandler',
+  'description' => 'The swiss army knife for all kinds of mailforms, completely new written using the MVC concept. Result: Flexibility, Flexibility, Flexibility  :-).',
+  'category' => 'plugin',
+  'author' => 'Dev-Team Typoheads',
+  'author_email' => 'dev@typoheads.at',
+  'author_company' => 'Typoheads GmbH',
+  'state' => 'stable',
+  'internal' => '',
+  'uploadfolder' => '0',
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '9.0.0',
+  'constraints' => [
+    'depends' => [
+      'typo3' => '^12.4',
+    ],
+    'conflicts' => [],
+    'suggests' => [],
+  ],
+];
---
+++ ext_localconf.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+defined('TYPO3') or exit;
+
+$boot = static function (): void {
+};
+
+$boot();
+unset($boot);
