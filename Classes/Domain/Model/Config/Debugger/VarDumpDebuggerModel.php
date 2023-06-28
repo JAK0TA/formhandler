b551cca21fb90f8bd3dcfaf6362202d206d8c107
Thomas Lüder <t.lueder@nets.de>
2023-06-28 22:32:37 +0200

    Added VarDumpDebugger

---
+++ Classes/Domain/Model/Config/Debugger/VarDumpDebuggerModel.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typoheads\Formhandler\Domain\Model\Config\Debugger;
+
+use Typoheads\Formhandler\Debugger\VarDumpDebugger;
+
+class VarDumpDebuggerModel extends AbstractDebuggerModel {
+  public readonly bool $active;
+
+  public readonly bool $ansiColors;
+
+  /** @var array<int, string> */
+  public readonly ?array $blacklistedClassNames;
+
+  /** @var array<int, string> */
+  public readonly ?array $blacklistedPropertyNames;
+
+  public readonly int $maxDepth;
+
+  public readonly bool $plainText;
+
+  public readonly bool $return;
+
+  public readonly ?string $title;
+
+  /**
+   * @param array<string, mixed> $config
+   */
+  public function __construct(array $config) {
+    $this->active = boolval($config['active'] ?? false);
+    $this->ansiColors = boolval($config['ansiColors'] ?? true);
+
+    if (isset($config['blacklistedClassNames']) && is_array($config['blacklistedClassNames'])) {
+      $this->blacklistedClassNames = $config['blacklistedClassNames'];
+    } else {
+      $this->blacklistedClassNames = null;
+    }
+
+    if (isset($config['blacklistedPropertyNames']) && is_array($config['blacklistedPropertyNames'])) {
+      $this->blacklistedPropertyNames = $config['blacklistedPropertyNames'];
+    } else {
+      $this->blacklistedPropertyNames = null;
+    }
+
+    $this->maxDepth = intval($config['maxDepth'] ?? 8);
+    $this->plainText = boolval($config['plainText'] ?? false);
+    $this->return = boolval($config['return'] ?? false);
+
+    if (isset($config['title'])) {
+      $this->title = strval($config['title']);
+    } else {
+      $this->title = null;
+    }
+  }
+
+  public function class(): string {
+    return VarDumpDebugger::class;
+  }
+}
