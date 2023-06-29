912ef4eaebd7d8a86300b3fa37d62f7744032734
Thomas Lüder <t.lueder@nets.de>
2023-06-29 23:49:03 +0200

    Added ErrorCheck MaxLength

--- Classes/Validator/ErrorCheck/MaxLength.php
+++ Classes/Validator/ErrorCheck/MaxLength.php
@@ -6,9 +6,22 @@ namespace Typoheads\Formhandler\Validator\ErrorCheck;
 
 use Typoheads\Formhandler\Domain\Model\Config\FormModel;
 use Typoheads\Formhandler\Domain\Model\Config\Validator\ErrorCheck\AbstractErrorCheckModel;
+use Typoheads\Formhandler\Domain\Model\Config\Validator\ErrorCheck\MaxLengthModel;
 
 class MaxLength extends AbstractErrorCheck {
   public function isValid(FormModel &$formConfig, AbstractErrorCheckModel &$maxLengthErrorCheckConfig, mixed $value): bool {
+    if (!$maxLengthErrorCheckConfig instanceof MaxLengthModel) {
+      return false;
+    }
+
+    if (is_string($value)
+        && mb_strlen(trim($value), 'utf-8') > 0
+        && $maxLengthErrorCheckConfig->maxLength > 0
+        && mb_strlen(trim($value), 'utf-8') > $maxLengthErrorCheckConfig->maxLength
+    ) {
+      return false;
+    }
+
     return true;
   }
 }
