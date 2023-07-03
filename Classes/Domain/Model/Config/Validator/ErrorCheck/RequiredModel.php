4335521f540c11bc67f857190aa21f1459a687f3
Thomas Lüder <t.lueder@nets.de>
2023-07-03 22:20:24 +0200

    Added more DocBocks

--- Classes/Domain/Model/Config/Validator/ErrorCheck/RequiredModel.php
+++ Classes/Domain/Model/Config/Validator/ErrorCheck/RequiredModel.php
@@ -6,6 +6,41 @@ namespace Typoheads\Formhandler\Domain\Model\Config\Validator\ErrorCheck;
 
 use Typoheads\Formhandler\Validator\ErrorCheck\Required;
 
+/** Documentation:Start:ErrorChecks/General/Required.rst.
+ *
+ *.. _required:
+ *
+ *========
+ *Required
+ *========
+ *
+ *Checks if a field is filled in
+ *
+ *..  code-block:: typoscript
+ *
+ *    Example Code:
+ *
+ *    validators {
+ *      DefaultValidator {
+ *        model = DefaultValidatorModel
+ *        config {
+ *          fields {
+ *            name.errorChecks {
+ *              required {
+ *                model = RequiredModel
+ *              }
+ *            }
+ *          }
+ *        }
+ *      }
+ *    }
+ *
+ *.. toctree::
+ *   :maxdepth: 2
+ *   :hidden:
+ *
+ *Documentation:End
+ */
 class RequiredModel extends AbstractErrorCheckModel {
   /**
    * @param array<string, mixed> $settings
