8d113e61d044b1035752daaf5d666b8057d3b65b
Nicklas Mantek <mantek@jakota.de>
2025-08-27 11:21:39 +0200

    support typo v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF['formhandler'] = [
   'version' => '9.0.0',
   'constraints' => [
     'depends' => [
-      'typo3' => '^12.4',
+      'typo3' => '12.4.0-13.4.99',
     ],
     'conflicts' => [],
     'suggests' => [],
